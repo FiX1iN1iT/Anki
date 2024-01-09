9220292a4de20554b9978d4e7d4711305dc588ca
fix1in1it <fix1in1it@gmail.com>
2024-01-09 19:12:57 +0300

    init app

---
+++ Anki/Anki/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Anki
+//
+//  Created by user on 09.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemMint
+    }
+}
+
